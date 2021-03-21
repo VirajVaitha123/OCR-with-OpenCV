7aade041f5002335419d134de2679853aa08f98c
Viraj Vaitha <viraj_vaitha@outlook.com>
2021-03-21 12:19:54 +0000

    Detect Key Points using ORB and import test forms

--- main.py
+++ main.py
@@ -14,5 +14,18 @@ Query_Image = Imgs + "/Query_Image.png"
 
 #### 2. Reading in Query Image
 ImgQ = cv2.imread(Query_Image)
+h,w,c = ImgQ.shape
+ImgQ = cv2.resize(ImgQ, (w//3,h//3))               # Rescaling Image (Takes up less space)
+
+#### 2. Preparing Detector
+orb = cv2.ORB_create(1000)                         # Number of Key Points (May need to iterate and display to find a good number)            
+kp1, des1 = orb.detectAndCompute(ImgQ,None)
+impKp1 = cv2.drawKeypoints(ImgQ,kp1,None) 
+#key_points and the unique points to the image, descriptors are the representation of these points making it easier to for the computer to understand
+
+
+
+#### 3. Reading in Query Image
+cv2.imshow("KeyPointsQuery",impKp1)         
 cv2.imshow("Output",ImgQ)                          # Output image
 cv2.waitKey(0)                                     # waitKey(0) will display the window infinitely until any keypress (it is suitable for image display).
