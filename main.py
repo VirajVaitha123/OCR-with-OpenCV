a09d67fa1a23ec845e1b3ce923b065711b797a69
Viraj Vaitha <viraj_vaitha@outlook.com>
2021-03-21 10:29:57 +0000

    Imported Libaries

---
+++ main.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as numpy
+import pytesseract
+
+# C:\Program Files\Tesseract-OCR
+
+pytesseract.pytesseract.Tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
