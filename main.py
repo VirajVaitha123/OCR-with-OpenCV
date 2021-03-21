1792eb5a67db73090e454665c58001b8e0e718d7
Viraj Vaitha <viraj_vaitha@outlook.com>
2021-03-21 11:46:28 +0000

    typo 1. -> 2.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ cwd = os.getcwd()
 Imgs = cwd + "/Images"
 Query_Image = Imgs + "/Query_Image.png"
 
-#### 1. Reading in Query Image
+#### 2. Reading in Query Image
 ImgQ = cv2.imread(Query_Image)
 cv2.imshow("Output",ImgQ)                          # Output image
 cv2.waitKey(0)                                     # waitKey(0) will display the window infinitely until any keypress (it is suitable for image display).
