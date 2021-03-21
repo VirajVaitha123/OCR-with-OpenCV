1f56c5200e1ee251557b97832557b999366b28ee
Viraj Vaitha <viraj_vaitha@outlook.com>
2021-03-21 11:15:31 +0000

    Read Image and display as a test to confirm it is working

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import numpy as np
 import cv2
-import numpy as numpy
 import pytesseract
 
 # C:\Program Files\Tesseract-OCR
 
 pytesseract.pytesseract.Tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+import os
+cwd = os.getcwd()
+print(cwd)
+
+ImgQ = cv2.imread("C:/Users/Viraj/Repos/OCR-with-OpenCV/Images/Query_Image.png")
+cv2.imshow("Output",ImgQ)
+#cv2.waitKey(0)
